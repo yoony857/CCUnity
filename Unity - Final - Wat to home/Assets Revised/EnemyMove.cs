14770531b7908ccce9f79f76d18d2aa69647572b
Young Tae Yoon <yoony857@newschool.edu>
2018-12-17 22:15:50 -0500

    Revised

---
+++ Unity - Final - Wat to home/Assets Revised/EnemyMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    int speedAmt = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= -27)
+        {
+            speedAmt = 10;
+        }
+
+        if (transform.position.x >= 27)
+        {
+            speedAmt = -10;
+        }
+
+        transform.Translate(speedAmt * Time.deltaTime, 0, 0);
+
+    }
+}
---
+++ Unity - Final - Wat to home/Assets Revised/Scoring.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour {
+
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        Score.theScore += 50;
+        Destroy(gameObject);
+    }
+}
